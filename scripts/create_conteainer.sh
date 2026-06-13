c75e33c118d00811b5003e9004df171b22250952
kani0dev <allanchagasilva@gmail.com>
2026-06-13 05:48:44 -0300

    enchancement - container now install python and update it self

--- scripts/create_conteainer.sh
+++ scripts/create_conteainer.sh
@@ -10,6 +10,7 @@ echo "Atualizando site..."
 sudo rm -rf rootfs/website
 sudo mkdir -p rootfs/website
 sudo cp -r website/* rootfs/website/
+sudo cp /etc/resolv.conf rootfs/etc/resolv.conf
 
 sudo unshare \
     --pid \
@@ -20,9 +21,13 @@ sudo unshare \
     --mount-proc \
     chroot "$ROOTFS" /bin/sh -c '
         export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-
+	echo "[1/3] Atualizando repositórios..."
         apk update
-        apk add python3
+
+        echo "[2/3] Instalando Python..."
+        apk add --no-cache python3
+
+        echo "[3/3] Subindo servidor..."
 
         exec /bin/sh
     '
