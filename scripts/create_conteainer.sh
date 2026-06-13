27c3d76d8d081800e9dcc48985018571b56185bd
kani0dev <allanchagasilva@gmail.com>
2026-06-13 05:39:49 -0300

    fix - the container was missing a dns config and python

--- scripts/create_conteainer.sh
+++ scripts/create_conteainer.sh
@@ -4,13 +4,25 @@ set -euo pipefail
 
 ROOTFS="$(realpath rootfs)"
 
+
+echo "Atualizando site..."
+
+sudo rm -rf rootfs/website
+sudo mkdir -p rootfs/website
+sudo cp -r website/* rootfs/website/
+
 sudo unshare \
-    --mount \
     --pid \
     --uts \
     --ipc \
-    --net \
     --cgroup \
     --fork \
     --mount-proc \
-    chroot "$ROOTFS" /bin/sh
+    chroot "$ROOTFS" /bin/sh -c '
+        export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+        apk update
+        apk add python3
+
+        exec /bin/sh
+    '
